a80c3666a4b2f2c93bb9043a920ad58ce9c55e8a
DrkSephy <dleonar00@citymail.cuny.edu>
2015-09-14 16:19:03 -0400

    Redirecting directly to bank tempalte after checkout

--- set/app/urls.py
+++ set/app/urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns('',
 	url(r'^register/$', views.register, name='register'),
 	url(r'^login/$', views.user_login, name='login'),
 	url(r'^shopping/$', views.shopping, name='shopping'),
-	url(r'^checkout/$', views.checkout, name='checkout'),
 	url(r'^bank/$', views.bank, name='bank'),
 	url(r'^transaction/$', views.transaction, name='transaction'),
 )
