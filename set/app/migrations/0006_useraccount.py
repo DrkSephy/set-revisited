18216604989f01769469d92b6c3bf939564a110d
DrkSephy <dleonar00@citymail.cuny.edu>
2015-09-14 20:00:07 -0400

    Adding UserAccount model, adding migrations

---
+++ set/app/migrations/0006_useraccount.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0005_auto_20150912_2232'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserAccount',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('account', models.DecimalField(max_digits=6, decimal_places=2)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- set/app/models.py
+++ set/app/models.py
@@ -2,27 +2,37 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
+	user = models.OneToOneField(User)
 
-    def __unicode__(self):
-        return self.user.username
+	def __unicode__(self):
+		return self.user.username
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+	name = models.CharField(max_length=50)
+	price = models.DecimalField(max_digits=6, decimal_places=2)
 
-    def __unicode__(self):
-        return self.name
+	def __unicode__(self):
+		return self.name
 
-    def __unicode__(self):
-        return str(self.price)
+	def __unicode__(self):
+		return str(self.price)
 
 class Vendor(models.Model):
-    name = models.CharField(max_length=50)
-    account = models.DecimalField(max_digits=6, decimal_places=2)
+	name = models.CharField(max_length=50)
+	account = models.DecimalField(max_digits=6, decimal_places=2)
 
-    def __unicode__(self):
-        return self.name
+	def __unicode__(self):
+		return self.name
 
-    def __unicode__(self):
-        return str(self.account)
+	def __unicode__(self):
+		return str(self.account)
+
+class UserAccount(models.Model):
+	name = models.CharField(max_length=50)
+	account = models.DecimalField(max_digits=6, decimal_places=2)
+
+	def __unicode__(self):
+		return self.name
+
+	def __unicode__(self):
+		return str(self.account)
